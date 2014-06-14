abdb55301fbcbc86cae2ebdc021b989c1eaad33a
ozw1z5rd@gmail.com <ozw1z5rd@gmail.com@b8a90e64-14a5-9039-6457-c4063f25d82e>
2014-06-14 22:44:51 +0000

    +legalese su TSC
    git-svn-id: https://gdg-pescara.googlecode.com/svn/trunk@31 b8a90e64-14a5-9039-6457-c4063f25d82e

--- web/DEE2014/TimespaceCapsule/models.py
+++ web/DEE2014/TimespaceCapsule/models.py
@@ -1,4 +1,25 @@
 # -*- coding: latin-1 -*-
+
+#     This file is part of Timespace Capsule.
+#
+#     Timespace Capsule is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     Timespace Capsule is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with Timespace Capsule.  If not, see <http://www.gnu.org/licenses/>.
+#
+#     Alessio Palma / 2014
+#     fb.com/alessio.palma
+#     https://sites.google.com/site/ozw1z5rd/
+
+
 from google.appengine.ext import db
 from datetime import datetime
 import math
--- web/DEE2014/TimespaceCapsule/template/AddCapsule.py
+++ web/DEE2014/TimespaceCapsule/template/AddCapsule.py
@@ -1,3 +1,22 @@
+#     This file is part of Timespace Capsule.
+#
+#     Timespace Capsule is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     Timespace Capsule is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with Timespace Capsule.  If not, see <http://www.gnu.org/licenses/>.
+#
+#     Alessio Palma / 2014
+#     fb.com/alessio.palma
+#     https://sites.google.com/site/ozw1z5rd/
+
 from template import TemplateBase
 
 class AddCapsuleFormTemplate(TemplateBase):
