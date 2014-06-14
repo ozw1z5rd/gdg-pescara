93ce1a49311b3e925256e6978491d1403c9ae223
ozw1z5rd@gmail.com <ozw1z5rd@gmail.com@b8a90e64-14a5-9039-6457-c4063f25d82e>
2014-06-14 22:47:52 +0000

    +legalese
    git-svn-id: https://gdg-pescara.googlecode.com/svn/trunk@32 b8a90e64-14a5-9039-6457-c4063f25d82e

--- web/DEE2014/TalkingPhotos/template/visitorPageTemplate.py
+++ web/DEE2014/TalkingPhotos/template/visitorPageTemplate.py
@@ -1,3 +1,22 @@
+#     This file is part of Talking Photos.
+#
+#     Talking Photos is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     Talking Photos is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with Talking Photos.  If not, see <http://www.gnu.org/licenses/>.
+#
+#     Alessio Palma / 2014
+#     fb.com/alessio.palma
+#     https://sites.google.com/site/ozw1z5rd/
+
 from template import TemplateBase
 
 # if user goes is here is logged ( authenticated by the app )
