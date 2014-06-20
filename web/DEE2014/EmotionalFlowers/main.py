2bf72c309ea5ada680c12c3f5e4e70c66f2660d7
ozw1z5rd@gmail.com <ozw1z5rd@gmail.com@b8a90e64-14a5-9039-6457-c4063f25d82e>
2014-06-20 20:22:27 +0000

    +style
    git-svn-id: https://gdg-pescara.googlecode.com/svn/trunk@41 b8a90e64-14a5-9039-6457-c4063f25d82e

--- web/DEE2014/EmotionalFlowers/main.py
+++ web/DEE2014/EmotionalFlowers/main.py
@@ -17,13 +17,48 @@
 #
 import webapp2
 
-class PaginaUnicai(webapp2.RequestHandler):
+class PaginaUnica(webapp2.RequestHandler):
     def get(self):
         self.response.write("""
 
-<!DOCTYPE HTML>
-<html>
-</html>
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="utf-8">
+				<meta http-equiv="X-UA-Compatible" content="IE=edge">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<meta name="description" content="">
+
+                <title>TemplateBase head</title>
+
+				<!-- Bootstrap core CSS -->
+				<link href="css/bootstrap.css" rel="stylesheet">
+
+				<!-- Custom styles for this template -->
+				<link href="css/style.css" rel="stylesheet">
+
+				<!-- Google web Font -->
+				<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,100'  rel='stylesheet' type='text/css'>
+
+				<!-- Just for debugging purposes. Don't actually copy this line! -->
+				<!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+				<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+				<!--[if lt IE 9]>
+					  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+					  <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+				<![endif]-->
+            </head>
+			<body>        
+				<header>
+					<p><a href="http://www.gdg-pescara.org" title="Google Developer Group Pescara Official Site"><img src="img/gdg-pescara.png" class="img-responsive"></a></p>
+					<h1><strong>DEE 2014</strong> // Timescape Capsule</h1>
+				</header>
+				<div class="container">
+				
+				</div>
+			</body>
+			</html>
         """);
 
 app = webapp2.WSGIApplication([
